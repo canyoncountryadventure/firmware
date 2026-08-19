e0de3b17744859e04d9ae06b5aec04a016481b35
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-08-19 15:30:46 -0600

    Route RAK4631 module hook to universal HOBO reader

--- src/modules/Telemetry/MX2001Diagnostic.h
+++ src/modules/Telemetry/MX2001Diagnostic.h
@@ -4,35 +4,8 @@
 
 #if defined(ARCH_NRF52) && defined(RAK_4631)
 
-#include "concurrency/OSThread.h"
-#include "mesh/SinglePortModule.h"
+#include "modules/Telemetry/HOBOMX2001MX2201MX2203/HOBOMX2001MX2201MX2203Telemetry.h"
 
-class MX2001DiagnosticModule :
-    public SinglePortModule,
-    private concurrency::OSThread
-{
-  public:
-    MX2001DiagnosticModule();
-
-  protected:
-    int32_t runOnce() override;
-
-    bool wantPacket(
-        const meshtastic_MeshPacket *p) override;
-
-    ProcessMessage handleReceived(
-        const meshtastic_MeshPacket &mp) override;
-
-  private:
-    bool sendMeasurementPacket(
-        float stageFeet,
-        float temperatureF,
-        uint16_t temperatureRaw);
-
-    bool sendTextReply(
-        uint32_t destination,
-        uint8_t channel,
-        const char *text);
-};
+using MX2001DiagnosticModule = HOBOMX2001MX2201MX2203TelemetryModule;
 
 #endif
