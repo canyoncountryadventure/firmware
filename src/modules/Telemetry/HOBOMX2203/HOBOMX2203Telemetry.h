4cc01226f5a333c8cc952a0345d345c14e432a8c
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-08-19 13:32:36 -0600

    Add production MX2203 telemetry module header

---
+++ src/modules/Telemetry/HOBOMX2203/HOBOMX2203Telemetry.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "configuration.h"
+
+#if defined(ARCH_NRF52) && defined(SEEED_XIAO_NRF52840_KIT)
+
+#include "concurrency/OSThread.h"
+#include "mesh/SinglePortModule.h"
+
+class HOBOMX2203TelemetryModule :
+    public SinglePortModule,
+    private concurrency::OSThread
+{
+  public:
+    HOBOMX2203TelemetryModule();
+
+  protected:
+    int32_t runOnce() override;
+
+    bool wantPacket(
+        const meshtastic_MeshPacket *p) override;
+
+    ProcessMessage handleReceived(
+        const meshtastic_MeshPacket &mp) override;
+
+  private:
+    bool sendTextReply(
+        uint32_t destination,
+        uint8_t channel,
+        const char *text);
+};
+
+#endif
