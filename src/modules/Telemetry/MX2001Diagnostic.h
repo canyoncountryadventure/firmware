f8a49f40e77e59d06fadec8060336eff16f79ab8
canyoncountryadventure <canyoncountryadventure@gmail.com>
2026-08-19 15:46:11 -0600

    Route RAK4631 module hook to proven MX2201 raw reader

--- src/modules/Telemetry/MX2001Diagnostic.h
+++ src/modules/Telemetry/MX2001Diagnostic.h
@@ -4,8 +4,10 @@
 
 #if defined(ARCH_NRF52) && defined(RAK_4631)
 
-#include "modules/Telemetry/HOBOMX2001MX2201MX2203/HOBOMX2001MX2201MX2203Telemetry.h"
-
-using MX2001DiagnosticModule = HOBOMX2001MX2201MX2203TelemetryModule;
+// Temporary RAK4631 MX2201 raw-capture validation route.
+// Modules.cpp already instantiates MX2001DiagnosticModule on RAK4631; alias
+// that hook to the older hardware-proven MX2201 reader for this debug branch.
+#include "modules/Telemetry/MX2201Telemetry.h"
+using MX2001DiagnosticModule = MX2201TelemetryModule;
 
 #endif
