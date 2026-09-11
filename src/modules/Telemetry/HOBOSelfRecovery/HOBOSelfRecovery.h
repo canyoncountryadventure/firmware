0f7b0b33b9a10f8bd374b895ff279617f38423c3
Canyon Country <293924291+canyoncountryadventure@users.noreply.github.com>
2026-09-10 21:43:39 -0600

    Add HOBO self-recovery supervisor interface

---
+++ src/modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "configuration.h"
+
+#if defined(ARCH_NRF52) && (defined(SEEED_XIAO_NRF52840_KIT) || defined(RAK_4631))
+
+#include "concurrency/OSThread.h"
+#include "mesh/SinglePortModule.h"
+
+class HOBOSelfRecoveryModule : public SinglePortModule, private concurrency::OSThread
+{
+  public:
+    HOBOSelfRecoveryModule();
+
+  protected:
+    int32_t runOnce() override;
+    bool wantPacket(const meshtastic_MeshPacket *p) override;
+    ProcessMessage handleReceived(const meshtastic_MeshPacket &mp) override;
+
+  private:
+    bool sendTextReply(uint32_t destination, uint8_t channel, const char *text);
+};
+
+#endif
