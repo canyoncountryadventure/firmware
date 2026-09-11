449f4ec6e8c0695e3e09bfaddcc51466a490eef8
Canyon Country <293924291+canyoncountryadventure@users.noreply.github.com>
2026-09-10 21:44:37 -0600

    Attach self-recovery supervisor to RAK4631 HOBO module

--- src/modules/Telemetry/MX2001Diagnostic.h
+++ src/modules/Telemetry/MX2001Diagnostic.h
@@ -5,7 +5,17 @@
 #if defined(ARCH_NRF52) && defined(RAK_4631)
 
 #include "modules/Telemetry/HOBOMX2001MX2201MX2203/HOBOMX2001MX2201MX2203Telemetry.h"
+#include "modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.h"
 
-using MX2001DiagnosticModule = HOBOMX2001MX2201MX2203TelemetryModule;
+// Preserve the existing Meshtastic RAK hook name while running the same
+// universal MX2001/MX2201/MX2203 reader plus the self-recovery supervisor.
+class MX2001DiagnosticModule : public HOBOMX2001MX2201MX2203TelemetryModule
+{
+  public:
+    MX2001DiagnosticModule() : HOBOMX2001MX2201MX2203TelemetryModule()
+    {
+        new HOBOSelfRecoveryModule();
+    }
+};
 
 #endif
