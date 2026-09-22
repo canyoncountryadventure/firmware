f9579f36d37e3fad56f8db023925a0f326f2e01e
Canyon Country <293924291+canyoncountryadventure@users.noreply.github.com>
2026-09-21 22:13:51 -0600

    Make VERSION identify exact V3 station hardware and sensors (RAK-HOBO-Safe-v3)

--- src/modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.cpp
+++ src/modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.cpp
@@ -19,7 +19,7 @@ namespace
 {
 static constexpr uint32_t BOOT_SETTLE_MS = 30000UL;
 static constexpr uint32_t SUPERVISOR_INTERVAL_MS = 30000UL;
-static constexpr char FIRMWARE_LABEL[] = "HOBO FIELD-RECOVERY v3";
+static constexpr char FIRMWARE_LABEL[] = "RAK HOBO SAFE V3";
 
 uint32_t commandRecoveryCount = 0;
 uint32_t resetReasonAtBoot = 0;
@@ -108,8 +108,8 @@ ProcessMessage HOBOSelfRecoveryModule::handleReceived(const meshtastic_MeshPacke
     }
 
     if (isCommand(payload, payloadSize, "VERSION")) {
-        snprintf(reply, sizeof(reply), "%s\nPlatform:%s\nSX1262+BLE+dual-WDT+12h safety reset",
-                 FIRMWARE_LABEL, platformName());
+        snprintf(reply, sizeof(reply), "%s\nHW:%s\nSensors:%s\nHOBO poll:30s | DFU:ON",
+                 FIRMWARE_LABEL, platformName(), "HOBO MX2001/2201/2203");
         sendTextReply(mp.from, mp.channel, reply);
         return ProcessMessage::CONTINUE;
     }
