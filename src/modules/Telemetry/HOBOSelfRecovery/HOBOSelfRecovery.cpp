f3c108251d19264085e7bd0c080e807aeaa7e767
Canyon Country <293924291+canyoncountryadventure@users.noreply.github.com>
2026-09-21 22:13:55 -0600

    Make VERSION identify exact V3 station hardware and sensors (RAK-Water-Distance-HOBO-v3)

--- src/modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.cpp
+++ src/modules/Telemetry/HOBOSelfRecovery/HOBOSelfRecovery.cpp
@@ -19,7 +19,7 @@ namespace
 {
 static constexpr uint32_t BOOT_SETTLE_MS = 30000UL;
 static constexpr uint32_t SUPERVISOR_INTERVAL_MS = 30000UL;
-static constexpr char FIRMWARE_LABEL[] = "RAK WATER DISTANCE + HOBO v3";
+static constexpr char FIRMWARE_LABEL[] = "RAK WATER DISTANCE + HOBO V3";
 
 uint32_t commandRecoveryCount = 0;
 uint32_t resetReasonAtBoot = 0;
@@ -108,8 +108,8 @@ ProcessMessage HOBOSelfRecoveryModule::handleReceived(const meshtastic_MeshPacke
     }
 
     if (isCommand(payload, payloadSize, "VERSION")) {
-        snprintf(reply, sizeof(reply), "%s\nPlatform:%s\nHOBO poll:30s | Water:ON\nDM: WATER STATUS / WATER RAW",
-                 FIRMWARE_LABEL, platformName());
+        snprintf(reply, sizeof(reply), "%s\nHW:%s\nSensors:%s\nHOBO poll:30s | DFU:ON",
+                 FIRMWARE_LABEL, platformName(), "HOBO + WATER DISTANCE");
         sendTextReply(mp.from, mp.channel, reply);
         return ProcessMessage::CONTINUE;
     }
